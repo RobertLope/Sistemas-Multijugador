28fd3a6d2afdadd455c8aa2fc4b38d2e801cd521
Robert <robertlopezcontreras@gmail.com>
2025-12-04 18:17:15 +0100

    Server updates

---
+++ Treball Multijugador Bloc 2/Servidor/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Samples;
+using Unity.Networking.Transport.Utilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        print(canvas);
+        /*
+        if (ServerBehaviour.Instance.perro == true) {
+
+            Debug.Log("Perro visto");
+
+            Transform perroTransform = canvas.transform.Find("perroPersonaje");
+            if (perroTransform != null)
+            {
+                perroTransform.gameObject.SetActive(true);
+            }
+        }
+        if (ServerBehaviour.Instance.creeper == true)
+        {
+            Transform creeperTransform = canvas.transform.Find("creeperPersonaje");
+            if (creeperTransform != null)
+            {
+                creeperTransform.gameObject.SetActive(true);
+            }
+        }
+        */
+    }
+    private void Update()
+    {
+        
+    }
+}
